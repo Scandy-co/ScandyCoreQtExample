095307c5b8a9e3481dbc39b24cf0d2ea99aea581
H. Cole Wiley <cole@scandy.co>
2017-03-21 03:16:48 -0500

    added in logging via scandycore and get major/minor/patch version of scandycore

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <scandy.h>
+#include <scandy/core/logging.h>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     ui->do_thing_2->hide();
+    int major, minor, patch;
+    scandy::core::getVersion(major, minor, patch);
+    scandy::core::logging::debug("hello from scandy!\nVersion: %d.%d.%d", major, minor, patch);
 }
 
 MainWindow::~MainWindow()
@@ -18,10 +24,12 @@ void MainWindow::on_do_thing_1_clicked()
 {
     ui->do_thing_1->hide();
     ui->do_thing_2->show();
+    scandy::core::logging::info("did thing1!");
 }
 
 void MainWindow::on_do_thing_2_clicked()
 {
     ui->do_thing_1->show();
     ui->do_thing_2->hide();
+    scandy::core::logging::warn("did thing2!");
 }
