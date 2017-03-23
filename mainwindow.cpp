f92e0f247df9e03d5bb24146219976ce16add964
H. Cole Wiley <cole@scandy.co>
2017-03-23 03:07:26 -0500

    updated example to launch a visualizer window with a test viewport. buttons relaunch the viewport at random sizes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,33 +2,64 @@
 #include "ui_mainwindow.h"
 
 #include <scandy.h>
+#include <scandy/core/IScandyCore.h>
+#include <scandy/core/visualizer/TestViewport.h>
+
 #include <iostream>
+
+using namespace scandy::core;
+
+std::shared_ptr<IScandyCore> scandycore;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-    ui->do_thing_2->hide();
-    int major, minor, patch;
-    scandy::core::getVersion(major, minor, patch);
-    std::cout << "hello from scandy!\nVersion: "
-      << major << "." << minor << "." << patch;
+  ui->setupUi(this);
+  ui->do_thing_2->hide();
+  int major, minor, patch;
+  scandy::core::getVersion(major, minor, patch);
+  std::cout << "Scansy Core Version: " << major << "." << minor << "." << patch << std::endl;
+
+  scandycore = IScandyCore::factoryCreate(400,400);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
 
 void MainWindow::on_do_thing_1_clicked()
 {
-    ui->do_thing_1->hide();
-    ui->do_thing_2->show();
+  ui->do_thing_1->hide();
+  ui->do_thing_2->show();
+
+  // Get a reference to the ScandyCore Visualizer
+  auto viz = scandycore->getVisualizer();
+  // Create a scandy::core::TestViewport
+  auto viewport = std::make_shared<TestViewport>();
+  // Add the test view port to the visualizer
+  viz->addViewport(*viewport);
+  // Start the visualizer's event loop
+  viz->start();
 }
 
 void MainWindow::on_do_thing_2_clicked()
 {
-    ui->do_thing_1->show();
-    ui->do_thing_2->hide();
+  // Get a reference to the ScandyCore Visualizer
+  auto viz = scandycore->getVisualizer();
+  // Stop the visualizer's event loop and join its thread
+  viz->stop();
+  viz->join();
+  // Reset the visualizer
+  viz = nullptr;
+
+  // And re-create the visualizer, but this time a random size
+  int width = 100+(800 * ((std::rand() % 100 )/ 100.0));
+  int height = 100+(800 * ((std::rand() % 100 )/ 100.0));
+  scandycore->createVisualizer(width,height);
+
+  // Switch the buttons back around
+  ui->do_thing_1->show();
+  ui->do_thing_2->hide();
 }
